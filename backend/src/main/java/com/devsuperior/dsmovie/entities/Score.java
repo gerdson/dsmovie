d74abb17e973d28240eb493c7621803a700b40e0
Gerdson Pinheiro <gerdsoncunha@gmail.com>
2022-01-12 16:37:50 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		this.getId().setMovie(movie);
+	}
+	
+	public void setuser(User user) {
+		this.getId().setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+}
